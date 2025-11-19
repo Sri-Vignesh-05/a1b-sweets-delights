0212cfcfb7713b2e90d2fa2613d20b9db61d9fea
sri-vignesh-05 <srivignesh.r@venpep.com>
2025-11-19 18:14:22 +0530

    7th change

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -2,9 +2,10 @@ const express6 = require('express');
 const prodCtrl = require('../controllers/productsController');
 const routerProd = express6.Router();
 
-
 routerProd.get('/', prodCtrl.getProducts);
 routerProd.get('/:id', prodCtrl.getProduct);
 routerProd.post('/', prodCtrl.createProduct);
 routerProd.put('/:id', prodCtrl.updateProduct);
 routerProd.delete('/:id', prodCtrl.deleteProduct);
+
+module.exports = routerProd;
